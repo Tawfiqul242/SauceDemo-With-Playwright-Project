a87d4633a3bb160bf85f374a5ff1282b445f1ea2
Tawfiqul Hasan <tawfiqul.abid@gmail.com>
2026-07-29 23:50:49 +0600

    updated locators & page action methods

--- pages/product_page.py
+++ pages/product_page.py
@@ -6,9 +6,15 @@ class ProductPage:
 
         #locators
         self.PRODUCTS_TITLE = page.get_by_text("Products", exact=True)
-        self.PRODUCTS = page.locator("div.inventory_list").locator("div")
+        self.PRODUCTS = page.locator(".inventory_item")
         self.MENU = page.get_by_role("button", name="Open Menu")
         self.LOGOUT_BTN = page.get_by_role("link", name="Logout")
+        self.IMAGE = page.locator("img.inventory_item_img")
+        self.ITEM_NAME = page.locator(".inventory_item_name")
+        self.ITEM_DESCRIPTION = page.locator("[data-test='inventory-item-desc']")
+        self.ITEM_PRICE = page.locator(".inventory_item_price")
+        self.ADD_TO_CART_BTN = page.get_by_role("button", name="Add to cart")
+        self.SORT_DROPDOWN = page.locator("[data-test='product-sort-container']")
 
     def click_menu(self):
         self.MENU.click()
@@ -16,4 +22,38 @@ class ProductPage:
     def click_logout_btn(self):
         self.LOGOUT_BTN.click()
 
-    
+    def click_on_product_name_by_index(self, index):
+        self.PRODUCTS.nth(index).locator(".inventory_item_name").click()
+
+
+    def get_products(self):
+        products = []
+
+        for item in self.PRODUCTS.all():
+            products.append({
+                "image": item.get_by_role("img"),
+                "name": item.locator(".inventory_item_name"),
+                "description": item.locator("[data-test='inventory-item-desc']"),
+                "price": item.locator(".inventory_item_price"),
+                "add_to_cart": item.get_by_role("button", name="Add to cart")
+                })
+        return products
+
+    def sort_name_by_ascending(self):
+        self.SORT_DROPDOWN.select_option("az")
+
+    def get_product_names(self):
+        return self.ITEM_NAME.all_inner_texts()
+
+    def sort_name_by_decending(self):
+        self.SORT_DROPDOWN.select_option("za")
+
+    def sort_by_price_low_to_high(self):
+        self.SORT_DROPDOWN.select_option("lohi")
+
+    def get_product_price(self):
+        prices = self.ITEM_PRICE.all_inner_texts()
+        return [float(price.replace("$", "")) for price in prices]
+
+    def sort_by_price_high_to_low(self):
+        self.SORT_DROPDOWN.select_option("hilo")
