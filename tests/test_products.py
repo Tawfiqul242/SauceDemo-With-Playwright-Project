e6c0526d3a4be18e750f901f8190ef09f526ef1e
Tawfiqul Hasan <tawfiqul.abid@gmail.com>
2026-08-06 00:05:59 +0600

    all product related tests are in test_products file

--- tests/test_products.py
+++ tests/test_products.py
@@ -1,6 +1,8 @@
 from playwright.sync_api import expect
 from pages.product_page import ProductPage
+from pages.product_details_page import Product_Details_Page
 
+# TC-003: Verify all products are displayed
 def test_all_product_displayed(page, logged_in):
     #class objects
     product_page = ProductPage(page)
@@ -18,3 +20,32 @@ def test_all_product_displayed(page, logged_in):
         expect(product["add_to_cart"]).to_be_visible()
 
 
+# TC-004: Verify product details page
+def test_product_details(page, logged_in):
+    # page class objects
+    product_page = ProductPage(page)
+    product_details = Product_Details_Page(page)
+
+    products = product_page.get_products()
+
+    expected_name = products[0]["name"].inner_text()
+    expected_description = products[0]["description"].inner_text()
+    expected_price = products[0]["price"].inner_text()
+
+    # select product to see product details
+    product_page.click_on_product_name_by_index(0)
+
+    # verify Product detail page opens
+    expect(product_details.PRODUCT_NAME).to_be_visible()
+
+    # Verify Correct name displayed.
+    expect(product_details.PRODUCT_NAME).to_have_text(expected_name)
+
+    # Verify Correct description displayed.
+    expect(product_details.PRODUCT_DESCRIPTION).to_have_text(expected_description)
+
+    # Verify Correct price displayed.
+    expect(product_details.PRODUCT_PRICE).to_have_text(expected_price)
+
+    # Verify Add to Cart button visible.
+    expect(product_details.ADD_TO_CART_BTN).to_be_visible()
