048be4103170eed8d1be4fca92975f4da944c1e1
Tawfiqul Hasan <tawfiqul.abid@gmail.com>
2026-08-16 13:07:16 +0600

    TC-012: Verify cart page displays selected products

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import expect
 from pages.product_page import ProductPage
+from pages.cart_page import CartPage
 
 
 # TC-009: Verify adding one product to cart
@@ -15,3 +16,34 @@ def test_add_one_product_to_cart(page, logged_in):
 
     # Button changes to Remove.
     expect(product_page.remove_btn_by_product_name("Sauce Labs Backpack")).to_be_visible()
+
+
+
+# TC-012: Verify cart page displays selected products
+def test_cart_page_displays_selected_products(page, logged_in):
+        
+    product_page = ProductPage(page)
+    cart_page = CartPage(page)
+
+    # Add two products
+    product_page.click_add_to_cart_btn_by_product_name("Sauce Labs Bolt T-Shirt")
+    product_page.click_add_to_cart_btn_by_product_name("Sauce Labs Fleece Jacket")
+
+    # Open cart
+    product_page.CART_LINK.click()
+
+    # Verify both products displayed
+    expect(cart_page.get_cart_product_by_name("Sauce Labs Bolt T-Shirt")).to_be_visible()
+    expect(cart_page.get_cart_product_by_name("Sauce Labs Fleece Jacket")).to_be_visible()
+
+    # Verify Name matches
+    expect(cart_page.get_cart_product_by_name("Sauce Labs Bolt T-Shirt")).to_have_text("Sauce Labs Bolt T-Shirt")
+    expect(cart_page.get_cart_product_by_name("Sauce Labs Fleece Jacket")).to_have_text("Sauce Labs Fleece Jacket")
+
+    # Verify Price matches
+    expect(cart_page.get_cart_product_price_by_name("Sauce Labs Bolt T-Shirt")).to_have_text("$15.99")
+    expect(cart_page.get_cart_product_price_by_name("Sauce Labs Fleece Jacket")).to_have_text("$49.99")
+
+    # Quantity = 1
+    expect(cart_page.get_cart_product_qty_by_name("Sauce Labs Bolt T-Shirt")).to_have_text("1")
+    expect(cart_page.get_cart_product_qty_by_name("Sauce Labs Fleece Jacket")).to_have_text("1")
